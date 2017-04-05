dc710ddca56ae713b5894c5c5b5f5f55bff536c9
Augustin Trancart <augustin.trancart@oslandia.com>
2017-04-05 17:37:23 +0200

    Chore: reformat webpack configs according to eslint rules

--- webpack-babel.config.js
+++ webpack-babel.config.js
@@ -1,20 +1,20 @@
 var path = require('path');
 
 module.exports = {
-  output: {
-    libraryTarget: 'commonjs2',
-    umdNamedDefine: true
-  },
-  module: {
-    loaders: [
-      // please consider modifying corresponding loaders in webpack.config.js too
-      {
-        test: /\.glsl$/,
-        include: [
-          path.resolve(__dirname, 'src'),
+    output: {
+        libraryTarget: 'commonjs2',
+        umdNamedDefine: true,
+    },
+    module: {
+        loaders: [
+            // please consider modifying corresponding loaders in webpack.config.js too
+            {
+                test: /\.glsl$/,
+                include: [
+                    path.resolve(__dirname, 'src'),
+                ],
+                loader: 'raw',
+            },
         ],
-        loader: 'raw'
-      },
-    ],
-  },
+    },
 };
